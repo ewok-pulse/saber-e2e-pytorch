d58001eb454378dab05d05d3415832720b850959
Ivor Wanders <ivor@iwanders.net>
2026-04-11 13:48:39 -0400

    Provide ability to retrieve exception information.

--- torch/csrc/shim_common.cpp
+++ torch/csrc/shim_common.cpp
@@ -742,3 +742,14 @@ AOTI_TORCH_EXPORT AOTITorchError torch_library_def_with_tags(
         torch::schema(schema), tag_vec, torch::_RegisterOrVerify::REGISTER);
   });
 }
+
+thread_local std::string torch_exception_what;
+thread_local std::string torch_exception_what_with_backtrace;
+
+const char* torch_exception_get_what() {
+  return torch_exception_what.c_str();
+}
+
+const char* torch_exception_get_what_with_backtrace() {
+  return torch_exception_what_with_backtrace.c_str();
+}
